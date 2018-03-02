2d92a436f675a041989207b3cc5995858f8e5e76
Briana Oursler <boursler@blackhole.localdomain>
2018-03-02 11:12:21 -0700

    introduce game state tracking with a variable to replace boolean functions, introduce test buttons, set up answer logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,31 +4,47 @@ var unattemptedQuestions = 0;
 var question = 0;
 var questionArr = [];
 var questionTimer;
-// function isTriviaInit() {
-// 	if (question === 0 && questionArr.length !== 0) {
-// 		return true;
-// 	}
-// 	else {
-// 		return false;
-// 	}
-// }
-//deal with states later
+//if gameInit, gameState = 0; if gameQuestion, gameState = 1; if gameAnswer, gameState = 2; if gameOver, gameState = 3
+var gameState = 0;
+var userInput;
+function initGame() {
+	gameState = 0;
+	question1 = new Question("Who is the main character in the Harry Potter series?", ["Draco Malfoy", "Roy Weasley", "Harry Potter"], "Harry Potter");
+	question2 = new Question("What color is the sky?"["Blue", "Green", "Purple"], "Blue");
+	question3 = new Question("Why do cats meow?"["to communicate with each other", "to talk to humans", "to entertain themselves"], "to talk to humans");
+	questionArr = [question1, question2, question3];
+}
+
 function answerTimer() {
-	questionTimer = setInterval(showAnswer(chooseQuestion())
-		, 100);
+	questionTimer = setInterval(displayTrivia, 1000);
 	var count = 0;
 	count++;
 	console.log("timer is running " + count);
 }
 function Question(questionBody, answerOptions, correctAnswer) {
+	//question constructor
 	this.questionBody = questionBody;
 	this.answerOptions = answerOptions;
 	this.correctAnswer = correctAnswer;
 }
 function chooseQuestion() {
+	gameState = 1;
 	question1 = new Question("Who is the main character in the Harry Potter series?", ["Draco Malfoy", "Ron Weasley", "Harry Potter"], "Harry Potter");
 	console.log("quesntion is " + question1.questionBody);
-	return question1;
+	question = question1;
+	return question;
+}
+function submitAnswer() {
+	gameState = 2;
+	if (userInput === question.correctAnswer) {
+		correctAnswers++
+	}
+	else if (userInput !== question.correctAnswer) {
+		incorrectAnswers++
+	}
+	else {
+		unattemptedQuestions++;
+	}
 }
 
 function showQuestion(question) {
@@ -41,15 +57,21 @@ function showQuestion(question) {
 function showAnswer(question) {
 	emptyDivs();
 	$("#questionDiv").html("<p>" + "The correct answer was " + question.correctAnswer + "</p>");
+	console.log(question.correctAnswer);
 }
 function emptyDivs() {
+	console.log("empty");
 	$("#questionDiv").empty();
 	$("#answerOptionsDiv").empty();
 }
 function displayTrivia() {
 	emptyDivs();
-	showQuestion(chooseQuestion());
-	answerTimer();
+	chooseQuestion();
+	showQuestion(question);
+	showAnswer(question);
 
 }
-displayTrivia();
+$("#testButton1").click(answerTimer);
+$("#testButton2").click(function clear() {
+	clearInterval(questionTimer);
+})
